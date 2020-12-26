f307cf9db44ab1ae237936edffe71f4dd21dbebd
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2020-12-26 12:36:33 +0200

    Starter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
-import Setup from "./Components/Places";
+import React, { useState } from "react";
+import Setup from "./Components/Menu";
 
 function App() {
+  const [all, setAll] = useState(true);
+  const [breakfast, setBreakfast] = useState(false);
+  const [lunch, setLunch] = useState(false);
+  const [dinner, setDinner] = useState(false);
   return (
     <div className="container">
       <Setup />
--- src/Components/reducer.js
+++
@@ -1,9 +0,0 @@
-export const reducer = (state, action) => {
-  if (action.type === "REMOVE_ITEM") {
-    const newPlaces = state.places.filter(
-      (place) => place.id !== action.payload
-    );
-    console.log(state.places);
-    return { ...state, places: newPlaces };
-  }
-};
